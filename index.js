2fae8f9ed2b5272c5c105c87623365e424c41728
tmoino <tony.moino@student.moringaschool.com>
2022-06-05 22:22:30 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const removeMain =document.getElementById("main");
+removeMain.remove();
+const newHeader=document.createElement("h1");
+newHeader.id= "victory";
+newHeader.textContent= "Tony is the champion";
+newHeader.className= "victorious";
